833eb5e9cc26def2316b23e8b11f6e857a118065
gurelnisanurr <nisanur.gurell28@gmail.com>
2025-04-26 16:46:45 +0300

    Update Zaman.java

--- Zaman.java
+++ Zaman.java
@@ -1,19 +1,42 @@
 package model;
 
-public class Zaman {
-    private String tarih;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public Zaman(String tarih) {
-        this.tarih = tarih;
+public class Zaman implements Comparable<Zaman> {
+    private LocalDateTime tarih;
+    private int saat;
+
+    public Zaman(String tarihStr, int saat) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.M.yyyy");
+        this.tarih = LocalDateTime.parse(tarihStr + " 00:00", DateTimeFormatter.ofPattern("d.M.yyyy HH:mm"));
+        this.tarih = this.tarih.plusHours(saat);
     }
 
-    public void saatEkle(int saat, int gunlukSaat) {
-        int gunEkle = saat / gunlukSaat;
-        tarih = "Gün: " + gunEkle;
+    public void birSaatIlerle() {
+        this.tarih = this.tarih.plusHours(1);
     }
 
-    public String getTarih() {
+    public String getTarihStr() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d.M.yyyy");
+        return tarih.format(formatter);
+    }
+
+    public int getSaat() {
+        return tarih.getHour();
+    }
+
+    public LocalDateTime getTarih() {
         return tarih;
     }
-}
 
+    @Override
+    public int compareTo(Zaman other) {
+        return this.tarih.compareTo(other.tarih);
+    }
+
+    @Override
+    public String toString() {
+        return getTarihStr() + " " + getSaat() + ". saat";
+    }
+}
